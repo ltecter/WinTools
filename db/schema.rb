600c513de22f79b9c4787a1e585e7e94a92ad199
Leonard <ltecter@academic.rrc.ca>
2013-04-09 14:54:42 -0500

    Added image and category id columns to the products model. Also created a categories model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404171658) do
+ActiveRecord::Schema.define(:version => 20130409194410) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(:version => 20130404171658) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "customers", :force => true do |t|
     t.text     "fname"
@@ -95,6 +101,8 @@ ActiveRecord::Schema.define(:version => 20130404171658) do
     t.integer  "stock_quantity"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.string   "image"
+    t.string   "category_id"
   end
 
   create_table "provinces", :force => true do |t|
